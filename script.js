ba3aaa20a4810a8fa459b0fcc5954a0ced5a285f
AName0 <115248908+AName0@users.noreply.github.com>
2023-07-29 20:31:28 +0100

    add operate function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let num1;
+let num2;
+let operator;
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -12,4 +16,16 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     return num1 / num2;
+}
+
+function operate(num1, num2, operator) {
+    if (operator === "+") {
+        return add(num1, num2);
+    } else if (operator === "-") {
+        return subtract(num1, num2);
+    } else if (operator === "*") {
+        return multiply(num1, num2);
+    } else if (operator === "/") {
+        return divide(num1, num2);
+    }
 }
