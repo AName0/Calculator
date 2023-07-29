add4c1067747797ec9f25f7c29f6bd4a01c16d54
AName0 <115248908+AName0@users.noreply.github.com>
2023-07-29 20:51:08 +0100

    wrote functions that willdiplay number on calc display

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let num1;
 let num2;
 let operator;
 
+const calculatorDisplay = document.getElementById("calculator-display")
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -28,4 +30,44 @@ function operate(num1, num2, operator) {
     } else if (operator === "/") {
         return divide(num1, num2);
     }
+}
+
+function displayNumZero() {
+    calculatorDisplay.textContent += "0";
+}
+
+function displayNumOne() {
+    calculatorDisplay.textContent += "1";
+}
+
+function displayNumTwo() {
+    calculatorDisplay.textContent += "2";
+}
+
+function displayNumThree() {
+    calculatorDisplay.textContent += "3";
+}
+
+function displayNumFour() {
+    calculatorDisplay.textContent += "4";
+}
+
+function displayNumFive() {
+    calculatorDisplay.textContent += "5";
+}
+
+function displayNumSix() {
+    calculatorDisplay.textContent += "6";
+}
+
+function displayNumSeven() {
+    calculatorDisplay.textContent += "7";
+}
+
+function displayNumEight() {
+    calculatorDisplay.textContent += "8";
+}
+
+function displayNumNine() {
+    calculatorDisplay.textContent += "9";
 }
